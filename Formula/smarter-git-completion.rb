498ac7885263f515c571510d8c4fc2dbbd97908b
Max Edmands <max@goodeggs.com>
2014-02-16 14:19:31 -0800

    Add formula for smarter-git-completion

---
+++ Formula/smarter-git-completion.rb
@@ -0,0 +1,11 @@
+require "formula"
+
+class SmarterGitCompletion < Formula
+  homepage "https://github.com/demands/smarter-git-completion"
+  url "https://github.com/demands/smarter-git-completion/archive/v0.1.0.tar.gz"
+  sha1 "118f7f8c089fa680401ac185cef52cb9ff122af1"
+
+  def install
+    bash_completion.install 'smarter-git-completion.bash'
+  end
+end
