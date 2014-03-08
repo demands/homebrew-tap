b7535af4a100f3204e6c9634fffd22db7386e509
Max Edmands <max@goodeggs.com>
2014-03-07 20:29:15 -0800

    Forgot to update to new binary / sha for mx

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Mx < Formula
   homepage "https://github.com/demands/mx"
-  url "https://github.com/demands/mx/archive/v0.4.2.tar.gz"
-  sha1 "499d9e38034fedd1617ab1820242f43cd4b82aa3"
+  url "https://github.com/demands/mx/archive/v0.4.3.tar.gz"
+  sha1 "af4c28737521a8397f5b4401949158ecb08a8665"
 
   def install
     install_dir = "#{prefix}/bin"
