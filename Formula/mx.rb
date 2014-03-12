ec414b1299f902535cdd5a440fe5d997c6515c03
Max Edmands <max@goodeggs.com>
2014-03-11 22:59:18 -0700

    Got rid of test for mx.
    This makes me sad, but right now mx takes over everything on your
    terminal, so a test would be pretty difficult. Maybe we should add
    a --version command to mx to make testing easier.

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -13,16 +13,4 @@ class Mx < Formula
 
     bash_completion.install 'mx-completion.bash'
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test mx`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "mx", "test_session"
-  end
 end
