88ccb23c101fe4e53aec931adc178beabea2ae5c
Max Edmands <me@maxedmands.com>
2014-07-06 09:13:34 -0700

    Update project to v0.1.0

--- Formula/project.rb
+++ Formula/project.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Project < Formula
   homepage 'https://github.com/demands/project'
-  url 'https://api.github.com/repos/demands/project/tarball/v0.0.1'
-  sha1 'fb059251b2a2a7d8bac662b7c604b50c3aaae79c'
+  url 'https://api.github.com/repos/demands/project/tarball/v0.1.0'
+  sha1 'b757fb2536ae26c9c952ff2c5b57e2c69682177c'
 
   def install
     install_dir = "#{prefix}/bin"
