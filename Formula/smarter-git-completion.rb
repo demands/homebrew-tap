9741e0e965b6626537d15d4681c364c390e00ce9
Max Edmands <me@maxedmands.com>
2014-07-16 12:59:52 -0700

    Bump smarter-git-completion to 0.2.0

--- Formula/smarter-git-completion.rb
+++ Formula/smarter-git-completion.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class SmarterGitCompletion < Formula
   homepage 'https://github.com/demands/smarter-git-completion'
-  url 'https://api.github.com/repos/demands/smarter-git-completion/tarball/v0.1.0'
-  sha1 '20952a18ea86fd97f5b54daf9cbe9c5dddabd6bf'
+  url 'https://api.github.com/repos/demands/smarter-git-completion/tarball/v0.2.0'
+  sha1 '5cbc9d0610f041ecee6db40f448c5a0bf914bdf9'
 
   def install
     bash_completion.install 'smarter-git-completion.bash' => "zzz-smarter-git"
