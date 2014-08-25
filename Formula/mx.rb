c10bd018682443eceb2c8ff0917a97ef0d2bef08
Max Edmands <me@maxedmands.com>
2014-08-24 21:05:40 -0400

    Add tmux dependency / a test to the mx formula

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -6,6 +6,8 @@ class Mx < Formula
   sha1 '01580b93cebe7d5496b8a76103d9eedf82a8c473'
 
   def install
+    depends_on "tmux"
+
     install_dir = "#{prefix}/bin"
     system "mkdir", "-p", install_dir
     system "cp", "./bin/mx", install_dir
@@ -14,5 +16,17 @@ class Mx < Formula
     bash_completion.install 'mx-completion.bash'
 
     man1.install "doc/mx.1"
+  end
+
+  test do
+    tmux = testpath/'tmux'
+    tmux.write 'echo $@'
+    tmux.chmod 0777
+
+    env = {
+      :PATH => "#{testpath}:#{ENV['PATH']}"
+    }
+
+    system env, 'mx .'
   end
 end
