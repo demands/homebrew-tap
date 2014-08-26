3957211cfa93a3d4481d65b9558e41a6c23401c1
Max Edmands <me@maxedmands.com>
2014-08-25 22:25:45 -0400

    Update mx

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -2,18 +2,18 @@ require "formula"
 
 class Mx < Formula
   homepage 'https://github.com/demands/mx'
-  url 'https://api.github.com/repos/demands/mx/tarball/v1.0.1'
-  sha1 '01580b93cebe7d5496b8a76103d9eedf82a8c473'
+  url 'https://api.github.com/repos/demands/mx/tarball/v1.1.0'
+  sha1 '30f8fb429943fff3178a8974afcf87bb8fde3795'
+  depends_on "tmux"
 
   def install
-    depends_on "tmux"
 
     install_dir = "#{prefix}/bin"
     system "mkdir", "-p", install_dir
     system "cp", "./bin/mx", install_dir
     system "chmod", "a+x", "#{install_dir}/mx"
 
-    bash_completion.install 'mx-completion.bash'
+    bash_completion.install './bin/mx-completion.bash'
 
     man1.install "doc/mx.1"
   end
